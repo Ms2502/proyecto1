b402cefe536899b7365945ecd39b3b03ccbfd88e
Ms2502 <you@example.com>
2022-06-17 21:27:00 -0300

    asd

---
+++ project1.py
@@ -0,0 +1,27 @@
+""" 1. Crear un codigo de python que:
+-crea un archivo en el que escribe numeros del 0 al 100 separados por un salto de linea. x
+
+2. Subir el codigo a un repositorio de github. <---
+
+3. Clonar el repositorio en una nueva carpeta.
+
+4. Crear un docker para el codigo creado en 1. El docker tiene que crear el archivo de python fuera del contendor.
+
+5. Actualizar el repositorio, para subir el dockerfile al repositorio
+
+6. Actualizar la carpeta donde se clono el repositorio """
+
+
+
+
+
+with open ("prueba1.txt","w") as file:
+    for n in range(1,101):
+        file.write(str(n) + '\n')
+    
+    
+    
+    
+    
+    
+
