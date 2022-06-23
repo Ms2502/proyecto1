9e450e3e04c61c78bd62ede99485c163caac8212
Ms2502 <maty.salinas@hotmail.com>
2022-06-22 22:44:28 -0300

    22/6 22hs

--- project1.py
+++ project1.py
@@ -13,11 +13,14 @@
 
 
 
-
+#Este script escribe numeros del 1 al 100 en un archivo .txt, luego crea el archivo "prueba1.txt" en la carpeta donde se ejecuta
 
 with open ("prueba1.txt","w") as file:
+    print("script ejecutandose...")
     for n in range(1,101):
         file.write(str(n) + '\n')
+
+print("listo :)")
     
     
     
